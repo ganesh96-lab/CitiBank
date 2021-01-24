63aea0ff2cc58ba364a24a3c2a9662451170908b
Tl <ganeshghodke7050@gmail.com>
2021-01-24 20:19:36 +0530

    DEv11 modified Test.java

--- demoProj1/src/com/ganesh/test/Test.java
+++ demoProj1/src/com/ganesh/test/Test.java
@@ -1,5 +1,11 @@
 package com.ganesh.test;
 
+import com.ganesh.beans.Utility;
+
 public class Test {
 
+	public static void main(String[] args) {
+		Utility util = new Utility();
+		System.out.println(util.getDate());
+	}
 }
